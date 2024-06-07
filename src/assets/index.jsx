7c4262860a791ef457b02da650661ac9d859e7e3
javid shabin <javid.main.act@gmail.com>
2024-06-07 23:24:31 +0530

    add contents on header components and add logo and styled for smal screens

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -3,11 +3,13 @@ import home from "./home.png";
 import search from "./search.png";
 import bag from "./bag.png";
 import user from "./user.png";
+import menu from "./menu.png";
 
 export{
     logo,
     home,
     search,
     bag,
-    user
+    user,
+    menu
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { logo } from '../assets'
+import { bag, home, logo, menu, search, user } from '../assets'
 
 export default function Header() {
   return (
-    <header>
+    <header className='header'>
         <div className="container">
             <div className="logo">
                 <img src={logo} alt="logo" />
+            </div>
+            <ul>
+                <li><a href="#"><img src={home} />Home</a></li>
+                <li><a href="#"><img src={search} />Search</a></li>
+                <li><a href="#"><img src={bag} />Cart</a></li>
+                <li><a href="#"><img src={user} />Account</a></li>
+            </ul>
+            <div className="toggle-menu">
+                <img src={menu} />
             </div>
         </div>
     </header>
