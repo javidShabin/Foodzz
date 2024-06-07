32130e520c5ac3dd88cd36c59c18d0f0d1d2bbc5
javid shabin <javid.main.act@gmail.com>
2024-06-08 00:22:21 +0530

    create home component

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 export default function Foods() {
   return (
     <div>
       <Header />
+      <Home/>
     </div>
   );
 }
