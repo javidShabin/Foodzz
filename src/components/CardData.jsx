94e60d386621c54ffa904a1d4759ff096a3abdeb
javid shabin <javid.main.act@gmail.com>
2024-06-08 08:57:49 +0530

    card data component create and add contents like array of object and export imorot the card data

---
+++ src/components/CardData.jsx
@@ -0,0 +1,17 @@
+import { slide1, slide2,slide3 } from "../assets";
+
+const cardData = [
+    {
+        id: 1,
+        imgSrc: slide1
+    },
+    {
+        id: 2,
+        imgSrc: slide2
+    },
+    {
+        id: 3,
+        imgSrc: slide3
+    }
+]
+export default cardData;
--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import cardData from "./CardData";
 
 
 export default function Foods() {
+    let home = cardData.map((item) => {
+        return(
+            <Home/>
+        )
+    })
   return (
     <div>
       <Header />
-      <Home/>
+      {home}
     </div>
   );
 }
