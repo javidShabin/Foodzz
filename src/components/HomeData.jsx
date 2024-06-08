a36b419c3e8ca0cce071150f0c49d9a36188b6ed
javid shabin <javid.main.act@gmail.com>
2024-06-08 08:34:53 +0530

    Create homeData components and import and export images like array of object format

---
+++ src/components/HomeData.jsx
@@ -0,0 +1,18 @@
+import { slide1, slide2, slide3 } from "../assets";
+
+const homeData = [
+    {
+        id: 1,
+        image: slide1
+    },
+    {
+        id: 2,
+        image: slide2
+    },
+    {
+        id: 3,
+        image: slide3
+    }
+]
+
+export default homeData;
