04eb33196c1c80f10382abd280e00d0c7cd0e202
javid shabin <javid.main.act@gmail.com>
2024-06-11 00:50:43 +0530

    create toggle menu bar and add show and showless function using useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 
 import { bag, home, logo, map, menu, search, user } from '../assets'
+import { useState } from 'react'
 
 export default function Header() {
+    let [showMenu, setShowMenu] = useState("")
+    const showMenuBar = () => {
+        setShowMenu(prevState => (prevState === "" ? "active" : ""))
+    }
   return (
     <header className='header'>
         <div className="container">
@@ -17,9 +22,21 @@ export default function Header() {
             <div className="location">
                 <span><img src={map}/><b>Tirur, Malappuram</b></span>
             </div>
-            <div className="toggle-menu">
-                <img src={menu}/>
+            {/* Toggle bar */}
+            <div className={`toggle-bar ${showMenu}`}>
+                <ol>
+                <li><a href="#"><img src={home} />Home</a></li>
+                <li><a href="#"><img src={search} />Search</a></li>
+                <li><a href="#"><img src={bag} />Cart</a></li>
+                <li><a href="#"><img src={user} />Account</a></li>
+                </ol>
+                <span><img src={map}/><b>Tirur, Malappuram</b></span>
             </div>
+            
+            <div className="toggle-menu">
+                <img src={menu} onClick={showMenuBar}/>
+            </div>
+            
         </div>
     </header>
   )
