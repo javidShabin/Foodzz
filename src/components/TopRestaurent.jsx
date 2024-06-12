bf67ed11b85bc24372ba023ecfe71853b1253423
javid shabin <javid.main.act@gmail.com>
2024-06-12 23:45:45 +0530

    add contennts and function for the top res conponent

--- src/components/TopRestaurent.jsx
+++ src/components/TopRestaurent.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { clock, discount, rest1, star } from "../assets";
 
 export default function TopRestaurent() {
   return (
-    <div>
-      <h1>This is Top restaurents Page</h1>
+    <div className="restaurent">
+      <div className="wrapper">
+        <div className="container">
+          <div className="card">
+            <div className="left">
+              <img src={rest1} />
+            </div>
+            <div className="right">
+              <h3>ok setakkam</h3>
+              <p>setakkam</p>
+              <div className="icon">
+                <span>
+                  <img src={star} />
+                  <b> 4.5</b>
+                </span>
+                <span>
+                  <img src={clock} />
+                  <b> 30 mins</b>
+                </span>
+              </div>
+              <span className="dicImg">
+                <img src={discount} />
+                <b> **UPTO 40% DICOUND</b>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
